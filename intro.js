439c0df8ceef96faaaf7a85451b1e4c674cdeca6
Dmajor81 <michelledimeji@gmail.com>
2026-05-16 07:58:39 +0100

    second commit: Betechified week one assignment

---
+++ intro.js
@@ -0,0 +1,7 @@
+const name = "Precious";
+const age = 22;
+const favoriteLanguage = "Node.js";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favorite Programming Language:", favoriteLanguage);
